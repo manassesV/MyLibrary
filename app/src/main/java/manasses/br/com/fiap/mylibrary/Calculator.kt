f71741aaa99fcc83e94a27913cfaca12546b1134
Logon Aluno <logonrmlocal@fiap.com.br>
2019-04-03 21:01:58 -0300

    Initial commit

---
+++ app/src/main/java/manasses/br/com/fiap/mylibrary/Calculator.kt
@@ -0,0 +1,13 @@
+package manasses.br.com.fiap.mylibrary
+
+class Calculator{
+
+    fun  somar(v1: Int, v2: Int): Int{
+        return v1 + v2
+    }
+
+    fun subtrair(v1: Int, v2: Int): Int{
+        return v1 - v2
+    }
+
+}
